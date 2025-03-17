b11144ba2413b6a9a37554726690f4468f026727
iijack-dotcom <cchhiv@deakin.edu.au>
2025-03-17 16:08:33 +1100

    Create Secrets.h

---
+++ Secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Jack"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Jack2112"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2880072			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "JT3ZV790ZLVS0QHS"   // replace XYZ with your channel write API Key
